f8c71f132da339b6675f0341dcede7d11e4a8cef
Rohit raj <rohit1201.be21@chitkara.edu.in>
2025-07-21 19:09:15 +0530

    added logo

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,25 +1,32 @@
 import { Button } from "./button";
+import Image from "next/image";
 
 interface AppbarProps {
-    user?: {
-        name?: string | null;
-    },
-    
-    onSignin: any,
-    onSignout: any
+  user?: {
+    name?: string | null;
+  };
+
+  onSignin: any;
+  onSignout: any;
 }
 
-export const Appbar = ({
-    user,
-    onSignin,
-    onSignout
-}: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4 border-slate-300">
-        <div className="text-lg flex flex-col justify-center font-semibold">
-            PayZO
-        </div>
-        <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
-        </div>
+export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
+  return (
+    <div className="flex justify-between border-b px-4 border-slate-300">
+      <div className="text-lg flex items-center justify-center font-semibold">
+        <div><Image
+          src="/images/payzo-logo.png"
+          alt="Profile Picture"
+          width={20}
+          height={20}
+        /></div>
+        <div className="text-[#040711]">PayZO</div>
+      </div>
+      <div className="flex flex-col justify-center pt-2">
+        <Button onClick={user ? onSignout : onSignin}>
+          {user ? "Logout" : "Login"}
+        </Button>
+      </div>
     </div>
-}
+  );
+};

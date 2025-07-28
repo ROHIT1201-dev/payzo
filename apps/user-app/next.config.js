153817e7ee19b995c7ed49d8d5b3d7bb1508145f
Rohit raj <rohit1201.be21@chitkara.edu.in>
2025-07-28 16:32:55 +0530

    fixed bugs

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+process.env.TSC_COMPILE_ON_ERROR = "true";
 module.exports = {
   transpilePackages: ["@repo/ui"],
   eslint: {
     ignoreDuringBuilds: true,
+
   },
 };
